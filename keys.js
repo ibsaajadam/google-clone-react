66c6eb9fa78a3975b37deadf399bd4d8c72c5103
iamibsaaj <ibsaa.adam1@gmail.com>
2021-10-31 16:40:12 -0400

    addded .env

--- keys.js
+++ keys.js
@@ -1,5 +1,7 @@
 // API Key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'AIzaSyDxGc0Lo-smGdgvwJvvFgM0dBBSYBH2Izw'
+// export const API_KEY = 'AIzaSyDxGc0Lo-smGdgvwJvvFgM0dBBSYBH2Izw'
+export const API_KEY = process.env.REACT_APP_API_KEY
 
 // Context Key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = 'a5e415f39179a5ff5'
+// export const CONTEXT_KEY = 'a5e415f39179a5ff5'
+export const CONTEXT_KEY = process.env.REACT_APP_CONTEXT_KEY
